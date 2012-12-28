ab88c26a46a9a0d8a244e8f6041381eb0282b640
Michael Lang <michael.lang@nexul.com>
2012-12-28 14:03:33 -0600

    Added installation capability to windows service
    Documentation on the install capability is documented here:
    
    http://candordeveloper.com/2012/12/28/simple-installer-for-windows-service-using-visual-studio-2012/

---
+++ Candor.Tasks.MultiWorkerService/_installer.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+echo Installing WindowsService...
+echo ---------------------------------------------------
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil /i Candor.Tasks.MultiWorkerService.exe
+echo ---------------------------------------------------
+echo Done.
+pause
---
+++ Candor.Tasks.MultiWorkerService/_uninstaller.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+echo Un-Installing WindowsService...
+echo ---------------------------------------------------
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil /u Candor.Tasks.MultiWorkerService.exe
+echo ---------------------------------------------------
+echo Done.
+pause
