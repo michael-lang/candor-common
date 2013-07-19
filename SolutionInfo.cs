4d1831145394f4562f62088c51dc49882d7990c6
Michael Lang <michael.lang@nexul.com>
2013-07-19 15:42:01 -0500

    NuGet release checkpoint 0.10.7.30719

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -13,4 +13,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.6.30719")]
+[assembly: AssemblyVersion("0.10.7.30719")]
